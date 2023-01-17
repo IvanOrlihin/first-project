e3bae64500dc3d1a8664ce8141e61d0cd262ce75
IvanOrlihin <orlihinivan@gmail.com>
2023-01-17 15:06:58 +0300

    Начало3

--- Converter.java
+++ Converter.java
@@ -1,1 +1,14 @@
-public class Converter() {
+public class Converter {
+    double calories == 0,05;
+    double kilometer == 0,0008;
+    
+    void convert(double stepsPerDay, int quantity) {
+        if (quantity == 1) {
+            System.out.println("Сегодня вы потеряли: " + calories * stepsPerDay + " килокалорий");
+        } else if (quantity == 2) {
+            System.out.println("Сегодня вы прошли: " + kilometer * stepsPerDay + "километров");
+        } else {
+            System.out.println("Неверный ввод, повторите команду");
+        }
+    }
+}
